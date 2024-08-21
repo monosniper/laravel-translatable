cf0f7026ba97eb28a06157b2c07ae46209965555
ravilto <vanteateam@gmail.com>
2024-08-21 17:39:09 +0500

    Update Translate.php

--- src/Livewire/Translate.php
+++ src/Livewire/Translate.php
@@ -166,11 +166,11 @@ class Translate extends Component
         }
     }
 
-    #[Layout('laravel-translatable::components.layouts.app')]
+    #[Layout('laravel-translatable-views::components.layouts.app')]
     public function render(): View
     {
         $this->dispatch('resize-textarea');
 
-        return view('laravel-translatable::livewire.translate');
+        return view('laravel-translatable-views::livewire.translate');
     }
 }
